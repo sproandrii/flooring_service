285d7364b935ab5ce6331b8be06ed2d30d8f5dfe
Andrii Slobodian <73819571+sproandrii@users.noreply.github.com>
2020-11-21 20:00:33 +0200

    scoop

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 document.body.innerHTML = '<h1>Hello from JS</h1>'
 console.log('Test from VS code')
+function minSum(arr) {
+    return arr.sort( (a,b) => a-b )
+    .slice(0, arr.length/2)
+    .reduce( (acc,item,index) => acc += item * arr[ arr.length - index - 1 ], 0 )
+    }
