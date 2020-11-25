d3791dfa40fe1bdea3034ad05f0810b72fce5de8
Andrii Slobodian <73819571+sproandrii@users.noreply.github.com>
2020-11-25 17:52:23 +0200

    still working on collapse

---
+++ carousel/carousel.js
@@ -0,0 +1,28 @@
+// const images = [`
+//     '<a href="#wood-species"><div style="width: 00px">
+//         <img src='img/services2/wood_floor_repairs.png' class="card-img-top" alt='Wood Floor Repair'>
+//          <h3 class="card-title">Wood Floor Repair</h3>
+//         <p class="card-text"><span>from $15</span> | per sqm</p>
+//     </div></a>'`,
+//     '<img src="img/flooring_options/10_mins_away_from_perfect_floor.jpeg" alt="first">',
+    // '<img src="img/flooring_options/flooring_installation_types.jpeg" alt="second">',
+    // '<imd src="img/flooring_options/flooring_options_and_services.jpeg" alt ="third">'
+
+    // 'img/flooring_options/10_mins_away_from_perfect_floor.jpeg',
+    // 'img/flooring_options/flooring_installation_types.jpeg',
+    // 'img/flooring_options/flooring_options_and_services.jpeg'
+// ]
+// let currentIndex = 0;
+
+// function slide () {
+//     // currentIndex++;
+//     // if (currentIndex > images.length - 1) currentIndex = 0;
+//     // document.querySelector('.carousel img').src =images[currentIndex];
+//     currentIndex = ++currentIndex > images.length - 1 ? 0 : currentIndex;
+//     showSlide(currentIndex);
+// }
+// setInterval(slide, 3000);
+
+// function showSlide(currentIndex) {
+//     document.querySelector('.carousel').innerHTML = images[currentIndex];
+// }
--- index.js
+++ index.js
@@ -27,23 +27,29 @@ setInterval( () => clock.innerText = new Date().toLocaleTimeString(), 1000)
 //     }
 // }, 1000)
 
-const newYear = new Date('Jan 01, 2021 00:00:00').getTime();
-const countdownToNewYear = setInterval( () => {
-    const currentTime = new Date().getTime();
-    const duration = newYear - currentTime;
-    const days = Math.floor(duration / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((duration % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((duration % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((duration % (1000 * 60)) / 1000);
-    if (duration > 0) {
-        document.querySelector('.new-year-label').innerText = 'New Year is comming'
-        document.querySelector('.countdown-to-new-year').innerText = `${days}d ${hours}h ${minutes}m ${seconds}s`;
-    } else {
-        document.querySelector('.countdown-to-new-year').hidden = true;
-        document.querySelector('.new-year-label').innerText = 'Classmates, Happy New Year!'
-    }
-}, 1000)
-
+// const newYear = new Date('Jan 01, 2021 00:00:00').getTime();
+// const countdownToNewYear = setInterval( () => {
+//     const currentTime = new Date().getTime();
+//     const duration = newYear - currentTime;
+//     const days = Math.floor(duration / (1000 * 60 * 60 * 24));
+//     const hours = Math.floor((duration % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//     const minutes = Math.floor((duration % (1000 * 60 * 60)) / (1000 * 60));
+//     const seconds = Math.floor((duration % (1000 * 60)) / 1000);
+//     if (duration > 0) {
+//         document.querySelector('.new-year-label').innerText = 'New Year is coming'
+//         document.querySelector('.countdown-to-new-year').innerText = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+//     } else {
+//         document.querySelector('.countdown-to-new-year').hidden = true;
+//         document.querySelector('.new-year-label').innerText = 'Classmates, Happy New Year!'
+//     }
+// }, 1000)
+// const test = [`
+//     <div>
+//         <p>Hay, what's up?</p>
+//     </div>`,
+//     'dsadasd',
+//     'sdsddsd'
+// ]
 
 
 // plusFiveSec =  new Date();
@@ -58,3 +64,43 @@ const countdownToNewYear = setInterval( () => {
 //         document.querySelector('.new-year').innerText = '';
 //     }
 // }, 1000)
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+const countdown = document.getElementById('countdown');
+const year = document.getElementById('year');
+const loading = document.getElementById('loading');
+
+const currentYear = new Date().getFullYear();
+
+const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+
+// Set background year
+// year.innerText = currentYear + 1;
+
+// Update countdown time
+function updateCountdown() {
+  const currentTime = new Date();
+  const diff = newYearTime - currentTime;
+
+  const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+  const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+  const m = Math.floor(diff / 1000 / 60) % 60;
+  const s = Math.floor(diff / 1000) % 60;
+
+  // Add values to DOM
+  days.innerHTML = d;
+  hours.innerHTML = h < 10 ? '0' + h : h;
+  minutes.innerHTML = m < 10 ? '0' + m : m;
+  seconds.innerHTML = s < 10 ? '0' + s : s;
+}
+
+// Show spinner before countdown
+setTimeout(() => {
+  loading.remove();
+  countdown.style.display = 'flex';
+}, 1000);
+
+// Run every second
+setInterval(updateCountdown, 1000);
