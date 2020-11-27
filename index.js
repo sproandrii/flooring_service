47c77ad22cb7ea1e8d8d7dbacdd568253d4ef7a1
Andrii Slobodian <73819571+sproandrii@users.noreply.github.com>
2020-11-27 16:53:43 +0200

    working on section OUR WORKS

--- index.js
+++ index.js
@@ -8,8 +8,8 @@
 // setInterval(() => blink.hidden = !blink.hidden, 2000)
 
 
-const clock = document.querySelector('.clock')
-setInterval( () => clock.innerText = new Date().toLocaleTimeString(), 1000)
+// const clock = document.querySelector('.clock')
+// setInterval( () => clock.innerText = new Date().toLocaleTimeString(), 1000)
 
 
 // const countdown = document.querySelector('.countdown');
@@ -64,43 +64,43 @@ setInterval( () => clock.innerText = new Date().toLocaleTimeString(), 1000)
 //         document.querySelector('.new-year').innerText = '';
 //     }
 // }, 1000)
-const days = document.getElementById('days');
-const hours = document.getElementById('hours');
-const minutes = document.getElementById('minutes');
-const seconds = document.getElementById('seconds');
-const countdown = document.getElementById('countdown');
-const year = document.getElementById('year');
-const loading = document.getElementById('loading');
+// const days = document.getElementById('days');
+// const hours = document.getElementById('hours');
+// const minutes = document.getElementById('minutes');
+// const seconds = document.getElementById('seconds');
+// const countdown = document.getElementById('countdown');
+// const year = document.getElementById('year');
+// const loading = document.getElementById('loading');
 
-const currentYear = new Date().getFullYear();
+// const currentYear = new Date().getFullYear();
 
-const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+// const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
 
-// Set background year
-// year.innerText = currentYear + 1;
+// // Set background year
+// // year.innerText = currentYear + 1;
 
-// Update countdown time
-function updateCountdown() {
-  const currentTime = new Date();
-  const diff = newYearTime - currentTime;
+// // Update countdown time
+// function updateCountdown() {
+//   const currentTime = new Date();
+//   const diff = newYearTime - currentTime;
 
-  const d = Math.floor(diff / 1000 / 60 / 60 / 24);
-  const h = Math.floor(diff / 1000 / 60 / 60) % 24;
-  const m = Math.floor(diff / 1000 / 60) % 60;
-  const s = Math.floor(diff / 1000) % 60;
+//   const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+//   const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+//   const m = Math.floor(diff / 1000 / 60) % 60;
+//   const s = Math.floor(diff / 1000) % 60;
 
-  // Add values to DOM
-  days.innerHTML = d;
-  hours.innerHTML = h < 10 ? '0' + h : h;
-  minutes.innerHTML = m < 10 ? '0' + m : m;
-  seconds.innerHTML = s < 10 ? '0' + s : s;
-}
+//   // Add values to DOM
+//   days.innerHTML = d;
+//   hours.innerHTML = h < 10 ? '0' + h : h;
+//   minutes.innerHTML = m < 10 ? '0' + m : m;
+//   seconds.innerHTML = s < 10 ? '0' + s : s;
+// }
 
-// Show spinner before countdown
-setTimeout(() => {
-  loading.remove();
-  countdown.style.display = 'flex';
-}, 1000);
+// // Show spinner before countdown
+// setTimeout(() => {
+//   loading.remove();
+//   countdown.style.display = 'flex';
+// }, 1000);
 
-// Run every second
-setInterval(updateCountdown, 1000);
+// // Run every second
+// setInterval(updateCountdown, 1000);
